8943fe62c7963338cbd33c3c33ce78265327cdc1
Denis.Karev <goofy.adun@gmail.com>
2022-11-10 14:21:56 +0300

    Объединение вещ и компл

--- Calculator/bot_funcs.py
+++ Calculator/bot_funcs.py
@@ -1,5 +1,5 @@
 import logging
-from bot_consts import MESSAGE_HI, KEYS_M, KEYS_1, KEYS_2
+from bot_consts import OPERATIONSV, MESSAGE_HI, KEYS_M, KEYS_1, KEYS_2, KEYS_C
 
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
 from telegram.ext import (
@@ -10,8 +10,8 @@ from telegram.ext import (
     ConversationHandler,
 )
 
-COMPLEX1, COMPLEX2, TOPMENU, CHOICE1, CHOICE2, INPUTV, FIRSTMENU, SECONDMENU, \
-SUMM, SUB, MULT, DIV, DIV_, REM, POW, SQRT = range(16)
+COMPLEX1, COMPLEX2, TOPMENU, CHOICE1, CHOICE2, INPUTV, INPUTV2, CONTMENU, FIRSTMENU, SECONDMENU, \
+SUMM, SUB, MULT, DIV, DIV_, REM, POW, SQRT = range(18)
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
@@ -56,89 +56,80 @@ def top_menu(update, context):
 
 def menu_one(update, context):
     text = update.message.text
-    print(text)
+    print('menu_one', text)
     context.user_data['operation'] = text
-    if context.user_data['operation'] == 'Квадратный корень' or context.user_data['operation'] == 'Предыдущее меню':
-        return 12
+    if context.user_data['operation'] == 'Квадратный корень':
+        context.user_data['n1'] = False
+        update.message.reply_text("Введите вещественное число:")
+        user = update.message.text
+        return INPUTV
+    elif context.user_data['operation'] == 'Предыдущее меню':
+        print(context.user_data['operation'])
+        markup_key = ReplyKeyboardMarkup(KEYS_M, one_time_keyboard=True)
+        update.message.reply_text(f'Главное меню!\n{MESSAGE_HI[66:]}', reply_markup=markup_key)
+        return TOPMENU
     else:
-        if context.user_data['numberv2'] == None:
-            # markup1_key = ReplyKeyboardMarkup(KEYS_1, one_time_keyboard=True)
-            update.message.reply_text("Введите вещ. число:", reply_markup=ReplyKeyboardRemove())
-            user = update.message.text
-            print(user)
-            return INPUTV
-        else:
-            if context.user_data['operation'] == 'Cумма':
-                context.user_data['result'] = context.user_data['numberv1']+context.user_data['numberv2']
-            elif context.user_data['operation'] == 'Разность':
-                context.user_data['result'] = context.user_data['numberv1'] - context.user_data['numberv2']
-            elif context.user_data['operation'] == 'Умножение':
-                context.user_data['result'] = context.user_data['numberv1'] * context.user_data['numberv2']
-            elif context.user_data['operation'] == 'Деление':
-                context.user_data['result'] = context.user_data['numberv1'] / context.user_data['numberv2']
-            elif context.user_data['operation'] == 'Целочисленное деление':
-                context.user_data['result'] = context.user_data['numberv1'] // context.user_data['numberv2']
-            elif context.user_data['operation'] == 'Деление с остатком':
-                context.user_data['result'] = context.user_data['numberv1'] / context.user_data['numberv2']
-            elif context.user_data['operation'] == 'Возведение в спепень':
-                context.user_data['result'] = context.user_data['numberv1'] ** context.user_data['numberv2']
-            elif context.user_data['operation'] == 'Квадратный корень':
-                context.user_data['result'] = context.user_data['numberv1'] ** 0.5
-            # elif context.user_data['operation'] == 'Предыдущее меню':
-            update.message.reply_text(f"Результат:{context.user_data['result']}", reply_markup=ReplyKeyboardRemove())
-            print(context.user_data['result'])
-            return TOPMENU
-
-
-
-# def menu_two(update, context):
-#     reply_keyboard = [['Cумма', 'Разность', 'Умножение'],
-#                       ['Деление', 'Целочисленное деление', 'Деление с остатком'],
-#                       ['Возведение в спепень', 'Квадратный корень'],
-#                       ['Предыдущее меню']]
-#     markup1_key = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
-#     update.message.reply_text(
-#         'Выберите арифметическое действие',
-#         reply_markup=markup1_key, )
-#     user = update.message.text
-#     print(user)
-#     return CHOICE1
+        context.user_data['n1'] = False
+        update.message.reply_text("Введите первое вещественное число:")
+    user = update.message.text
+    print(user)
+    return INPUTV2
 
 
 def menu_two(update, context):
     text = update.message.text
     print(text)
     context.user_data['operation'] = text
+    if context.user_data['operation'] == 'Предыдущее меню':
+        print(context.user_data['operation'])
+        markup_key = ReplyKeyboardMarkup(KEYS_M, one_time_keyboard=True)
+        update.message.reply_text(f'Главное меню!\n{MESSAGE_HI[66:]}', reply_markup=markup_key)
+        return TOPMENU
     update.message.reply_text("Введите первое комплексное число через пробел")
     return COMPLEX1
 
 
+def inputv_2number(update, context):
+    """Parsing a number"""
+    number = update.message.text
+    if not context.user_data['n1']:
+        try:
+            context.user_data['numberv1'] = float(number)
+        except:
+            update.message.reply_text('Непонятно, попробуйте еще раз')
+            return INPUTV2
+        context.user_data['n1'] = True
+        context.user_data['n2'] = False
+        update.message.reply_text('Введите второе вещественное число:')
+        print(context.user_data['numberv1'])
+        return INPUTV2
+    else:
+        try:
+            context.user_data['numberv2'] = float(number)
+        except:
+            update.message.reply_text('Непонятно, попробуйте еще раз')
+            return INPUTV2
+        context.user_data['n2'] = True
+        print(context.user_data['numberv2'])
+        print(context.user_data['operation'])
+        temp = OPERATIONSV[context.user_data['operation']](context.user_data['numberv1'], context.user_data['numberv2'])
+        update.message.reply_text(f'Результат операции: {temp}',
+                                    reply_markup=ReplyKeyboardMarkup(KEYS_C, one_time_keyboard=True))
+        return CONTMENU
+
+
 def inputv_number(update, context):
     """Parsing a number"""
-    text = update.message.text
+    number = update.message.text
     try:
-        context.user_data['number1'] = int(text)
+        context.user_data['numberv1'] = float(number)
     except:
-        update.message.reply_text(
-            'Not a number!!!',
-            reply_markup=ReplyKeyboardRemove)
+        update.message.reply_text('Непонятно, попробуйте еще раз')
         return INPUTV
-    print('Number !!!!')
-    return 1
-
-
-def inputv_number2(update, context):
-    """Parsing a number"""
-    text = update.message.text
-    try:
-        context.user_data['numberv2'] = int(text)
-    except:
-        update.message.reply_text(
-            'Not a number!!!',
-            reply_markup=ReplyKeyboardRemove)
-        return INPUTV
-    print('Number !!!!')
-    return
+    temp = OPERATIONSV[context.user_data['operation']](context.user_data['numberv1'])
+    update.message.reply_text(f'Результат операции: {temp}',
+                                reply_markup=ReplyKeyboardMarkup(KEYS_C, one_time_keyboard=True))
+    return CONTMENU
 
 
 def inputv_complex1(update, context):
@@ -146,7 +137,7 @@ def inputv_complex1(update, context):
     print("complex1_start")
     try:
         text = update.message.text.split()
-        context.user_data['complex1'] = complex(int(text[0]), int(text[1]))
+        context.user_data['complex1'] = complex(float(text[0]), float(text[1]))
     except:
         update.message.reply_text(
             'Not a Complex number !!!',
@@ -156,8 +147,9 @@ def inputv_complex1(update, context):
     if context.user_data['operation'] == 'Квадратный корень' and context.user_data['complex1'] != '':
          context.user_data['result'] = context.user_data['complex1'] ** 0.5
          update.message.reply_text(
-             f"квадрат корень ! из {context.user_data['complex1']} равен {context.user_data['result']}")
-         return TOPMENU
+             f"Квадрат корень ! из {context.user_data['complex1']} равен {context.user_data['result']}",
+                                reply_markup=ReplyKeyboardMarkup(KEYS_C, one_time_keyboard=True))
+         return CONTMENU
     update.message.reply_text("Ведите второе комплексное число")
     return COMPLEX2
 
@@ -167,7 +159,7 @@ def inputv_complex2(update, context):
     print("complex2_start")
     try:
         text = update.message.text.split()
-        context.user_data['complex2'] = complex(int(text[0]), int(text[1]))
+        context.user_data['complex2'] = complex(float(text[0]), float(text[1]))
     except:
         update.message.reply_text(
             'Not a Complexnumber !!!',
@@ -175,10 +167,11 @@ def inputv_complex2(update, context):
         return COMPLEX2
     print('Complex2_Number !!!!')
     result = comlex_operation(update, context)
-    update.message.reply_text(f" {context.user_data['operation']}комплексных чисел\n{context.user_data['complex1']}\n"
-                              f"{context.user_data['complex2']}\nРовна {result}")
-    update.message.reply_text(f"давай еще разок")
-    return TOPMENU
+    update.message.reply_text(f" {context.user_data['operation']} комплексных чисел\n{context.user_data['complex1']}\n"
+                              f"{context.user_data['complex2']}\nРавна {result}",
+                                reply_markup=ReplyKeyboardMarkup(KEYS_C, one_time_keyboard=True))
+    # update.message.reply_text(f"давай еще разок")
+    return CONTMENU
 
 
 
@@ -187,19 +180,18 @@ def echo(update, context):
     update.message.reply_text(update.message.text)
 
 
-def modules_menu(update, context):
-    """Echo the user message."""
-    update.message.reply_text(update.message.text)
-
-
-def input_first_num(update, context):
-    update.message.reply_text(
-        'Введите первое число')
-    user = update.message.from_user
-    update.message.reply_text(user)
-    print(user)
-    print(type(user))
-    return -1
+def cont_menu(update, context):
+    """Continue or not"""
+    text = update.message.text
+    if text == 'Еще разок':
+        markup_key = ReplyKeyboardMarkup(KEYS_M, one_time_keyboard=True)
+        update.message.reply_text(f'Главное меню!\n{MESSAGE_HI[66:]}', reply_markup=markup_key)
+        return TOPMENU
+    elif text == 'Выход':
+        update.message.reply_text('ОКи, приходите еще! %))')
+        return ConversationHandler.END
+    else:
+        logger.warning('Щось пошло не так')
 
 
 def cancel(update, context):
@@ -219,10 +211,10 @@ def cancel(update, context):
 
 def comlex_operation(update, context):
     text = update.message.text
-    com1 = context.user_data['complex2']
+    com1 = context.user_data['complex1']
     com2 = context.user_data['complex2']
     print(text)
-    update.message.reply_text(f"первое число{com1} второе число{com2} оператор {context.user_data['operation']}")
+    update.message.reply_text(f"Первое число{com1} второе число{com2} оператор {context.user_data['operation']}")
     operation = context.user_data['operation']
     if com2 != '':
         if operation == 'Cумма':
